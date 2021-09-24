e13bb01024d49a41f43c84a824b0c98ba8152406
mide7 <imharyomide@gmail.com>
2021-09-24 15:43:02 +0100

    nextjs project folder added

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,9 @@ import connect from "./db/connect"
 import dotenv from "dotenv"
 import productRoutes from "./routes/products.routes"
 import authRoutes from "./routes/auth.routes"
+import session from "express-session"
+import MongoDBStore from "connect-mongodb-session"
+MongoDBStore(session)
 
 
 const app: express.Application = express()
@@ -15,6 +18,14 @@ const host = process.env.HOST as string
 // --------------- MIDDLEWARES -----------------------
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(session({
+    secret: process.env.SESSIONSECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoDBStore({
+        uri: process.env.DBURI
+    })
+}))
 // ---------------- END OF MIDDLEARES ----------------
 
 
